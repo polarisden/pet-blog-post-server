d5ece658c738e511b818a535e6c382638e6875a9
polaris <dinsor1545@gmail.com>
2026-02-03 14:10:24 +0700

    feat: add dotenv and database connection, implement post creation and fetching endpoints

--- app.mjs
+++ app.mjs
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -7,9 +9,45 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello TechUp!");
+app.post("/posts", async (req, res) => {
+  const newPost = {
+    ...req.body
+  }
+  try {
+    await connectionPool.query(`
+      insert into posts (title, image, category_id, description, content, status_id)
+      values ($1, $2, $3, $4, $5, $6)
+      `,[
+        newPost.title,
+        newPost.image,
+        newPost.category_id,
+        newPost.description,
+        newPost.content,
+        newPost.status_id
+      ])
+
+    return res.status(200).json({
+      message: "Created post sucessfully"
+    })
+  } catch (e) {
+    res.status(500).json({ 
+      message: "Server could not create post because database connection" 
+    })
+    console.log("error =",e)
+  }  
 });
+
+app.get("/test", async (req, res) => {
+  try {
+    const result = await connectionPool.query("SELECT * FROM posts");
+    res.status(200).json(result.rows);
+  } catch (e) {
+    res.status(500).json({ message: "Server could not fetch posts because of database connection" });
+    console.log("error =", e);
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
--- utils/db.mjs
+++ utils/db.mjs
@@ -1,10 +1,11 @@
 // Create PostgreSQL Connection Pool here !
 import * as pg from "pg";
 const { Pool } = pg.default;
+import "dotenv/config"
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://postgres:Oatzazaide2@localhost:5432/build-complete-CRUD"
+  process.env.CONNECTION_STRING
 ,
 });
 
